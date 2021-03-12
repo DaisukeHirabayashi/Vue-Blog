3a1239c639211e17142bcd5fb590f73e88a22eb8
平林大典 <daisuke@pluslab.org>
2021-03-13 08:49:10 +0900

    最新の三件の表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,38 +16,53 @@
         </v-layout>
       </v-container>
     </v-img>
+    <v-container>
+      <v-toolbar color="cyan" dark>
+        <v-toolbar-title>最新の三件</v-toolbar-title>
+      </v-toolbar>
+      <v-list>
+        <v-list-item-group color="primary">
+          <template v-for="markdown_title in markdown_titles">
+            <v-list-item :key="markdown_title.markdown_key">
+              <v-list-item-icon>
+                <v-icon>{{"mdi-menu-right"}}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  v-text="markdown_title.markdown_text"
+                ></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider :key="`second-${markdown_title.markdown_key}`"></v-divider><!-- 区切り線 -->
+          </template>
+        </v-list-item-group>
+      </v-list>
+    </v-container>
   </v-app>
 </template>
 
 <script>
 
-import marked from "marked"
 import firebase from 'firebase'
 
 export default {
   data() {
     return {
+      markdown_titles: [],
       img_src:require("../assets/cherry-blossom.jpg")
     }
   },
   created() {
-    this.axios.get('./markdown.md')
-        .then(response => this.markdown_text = response.data);
-    const ref_markdown = firebase.database().ref('/markdown');//"-MVU3vgLypjxus0REDJ9"
-    ref_markdown.limitToLast(10).on('child_added', this.childAdded);
-
-    const dataRef = firebase.database().ref('/markdown/-MVaID_mMXoSeCGiKZbY');
-    dataRef.once("value")
-    .then(function(snapshot) {
-      document.querySelector('#content').innerHTML = marked(snapshot.child("markdown_text").val());
-    });
+    const ref_markdown_titles = firebase.database().ref('/mk_title');
+    ref_markdown_titles.limitToLast(3).on('child_added', this.childAdded);
   },
   methods: {
     childAdded(snap) {
       const markdown = snap.val()
-      this.markdown_texts.push({
+      this.markdown_titles.push({
         key: snap.key,
-        markdown_text: markdown.markdown_text
+        markdown_key:markdown.id,
+        markdown_text: markdown.markdown_title
       })
     },
     doSend() {
