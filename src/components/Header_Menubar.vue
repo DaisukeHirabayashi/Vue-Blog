f6398212df9169a56b2359fd0fef385ccb6fac18
平林大典 <daisuke@pluslab.org>
2021-03-15 22:24:16 +0900

    header情報の編集

--- src/components/Header_Menubar.vue
+++ src/components/Header_Menubar.vue
@@ -13,16 +13,65 @@
                     <v-list-item-title>{{ menu.name }}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
+            <v-list-item v-if="!$store.state.store_user.uid" @click="doLogin()">
+                <v-list-item-icon>
+                    <v-icon>{{'mdi-account-box'}}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title>Administrator Login</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item v-else @click="doLogout()">
+                <v-list-item-icon>
+                    <v-icon>{{'mdi-exit-to-app'}}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title>Logout</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
         </v-list>
     </v-menu>
 </template>
 
 <script>
+import firebase from 'firebase'
+import { mapMutations } from "vuex";
+import { USER_UPDATE } from "../store/mutation-types";
+
 export default {
   name: "Header_Menubar",
   props: {
     menu_lists: Array,
     menu_title: Object
+  },
+  computed: {
+    user_uid: function(){
+      return this.$store.state.store_user.uid;
+    }
+  },
+    methods: {
+    // ログイン処理
+    ...mapMutations({
+      USER_UPDATE
+    }),
+    doLogin() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider).then(result => {
+        // ログインしたユーザーの情報を取得します。
+        if(result.user.uid == "mQyHVZi87AMkrbR7PXXaFHQAHWb2"){
+          this.USER_UPDATE(result.user);
+          console.log(this.$store.state.store_user);
+        }
+      }).catch(function(err) {
+        console.error(err)
+        // エラー処理
+      });
+    },
+    doLogout() {
+      this.USER_UPDATE({});
+      console.log(this.$store.state.store_user);
+      alert("ログアウトしました");
+    },
   }
 };
 </script>
